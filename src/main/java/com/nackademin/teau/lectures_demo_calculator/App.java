4557e28db4357dd85d1faaa20524dfd300459668
Rafael Silva <rasilva.1986@gmail.com>
2020-03-28 16:13:23 +0100

    Second commit. Added a test for the project

--- src/main/java/com/nackademin/teau/lectures_demo_calculator/App.java
+++ src/main/java/com/nackademin/teau/lectures_demo_calculator/App.java
@@ -1,13 +1,8 @@
 package com.nackademin.teau.lectures_demo_calculator;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		Calculator c = new Calculator();
+		System.out.println(c.addition(2, 3));
+	}
 }
---
+++ src/test/java/com/nackademin/teau/lectures_demo_calculator/CalculatorTest.java
@@ -0,0 +1,15 @@
+package com.nackademin.teau.lectures_demo_calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	Calculator c = new Calculator();
+
+	@Test
+	public void testAddition() {
+		assertEquals(c.addition(2,1), 3, 0);
+	}
+
+}
