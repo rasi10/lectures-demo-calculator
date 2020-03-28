485ff9907ce6108a13f337b32cefbf0da38686f1
Rafael Silva <rasilva.1986@gmail.com>
2020-03-28 15:59:15 +0100

    First Commit. Just started the project

---
+++ src/main/java/com/nackademin/teau/lectures_demo_calculator/App.java
@@ -0,0 +1,13 @@
+package com.nackademin.teau.lectures_demo_calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/nackademin/teau/lectures_demo_calculator/AppTest.java
@@ -0,0 +1,20 @@
+package com.nackademin.teau.lectures_demo_calculator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
